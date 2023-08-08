65a4a908792792a3d3183d00eff840fc2dcce065
rickyra <rickyraihan83@gmail.com>
2023-08-08 08:29:39 +0700

    final-title

--- main.tsx
+++ main.tsx
@@ -3,7 +3,7 @@
 import blog, { ga, redirects, h } from "blog";
 
 blog({
-  title: "ricky ra",
+  title: "ricky_ra",
   // description: "This is my new blog.",
   // header: <header>Your custom header</header>,
   // section: (post: Post) => <section>Your custom section with access to Post props.</section>,
